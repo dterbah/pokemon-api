ce7db8c48169ace3d71e7df60bbeedcaad3c6565
Terba <dterba@ifa501-e03.insa-lyon.fr>
2019-10-27 12:29:17 +0100

    Move the Image Pagination in a new Component

--- src/components/PokemonBox.vue
+++ src/components/PokemonBox.vue
@@ -1,90 +1,73 @@
 <template>
-  <div class="card col box" style="width: 18rem;">
-    <div class="img-container border-bottom border-dark" @click="(e) => e.preventDefault()">
-      <nav aria-label="Page navigation example">
-        <ul class="pagination d-flex justify-content-center">
-          <li class="page-item">
-            <a class="page-link" href="#" aria-label="Previous">
-              <span aria-hidden="true" @click="previousImage">&laquo;</span>
-              <span class="sr-only">Previous</span>
-            </a>
-          </li>
-          <li class="page-item" v-for="(sprite, index) in sprites" :key="index">
-            <a class="page-link" href="#" @click="currentIndex = index">{{ index + 1 }}</a>
-          </li>
-          <li class="page-item">
-            <a class="page-link" href="#" aria-label="Next">
-              <span aria-hidden="true" @click="nextImage">&raquo;</span>
-              <span class="sr-only">Next</span>
-            </a>
-          </li>
-        </ul>
-      </nav>
-      <transition name="fade">
-        <img :src="currentImage" class="card-img-top" />
-      </transition>
+    <div class="card col box" style="width: 18rem;">
+        <pokemon-box-paginer @update-index="updateIndex" :nbrImages="spritesLength"></pokemon-box-paginer>
+        <div class="img-container border-bottom border-dark">
+            <transition name="fade">
+                <img :src="currentImage" class="card-img-top" />
+            </transition>
+        </div>
+        <div class="card-body">
+            <ul class="list card-text">
+                <li>Name : {{ capitalizedName }}</li>
+                <li>ID : {{ pokemon.id }}</li>
+                <li>Height : {{ pokemon.height }}</li>
+                <li>Base Experience : {{ pokemon.baseExperience}}</li>
+            </ul>
+        </div>
     </div>
-    <div class="card-body">
-      <ul class="list card-text">
-        <li>Name : {{ capitalizedName }}</li>
-        <li>ID : {{ pokemon.id }}</li>
-        <li>Height : {{ pokemon.height }}</li>
-        <li>Base Experience : {{ pokemon.baseExperience}}</li>
-      </ul>
-    </div>
-  </div>
 </template>
 
 <script>
+import PokemonBoxPaginer from "./PokemonBoxPaginer.vue";
 export default {
-  props: {
-    pokemon: { type: Object }
-  },
-  data() {
-    return {
-      currentIndex: 0
-    };
-  },
-  computed: {
-    currentImage: function() {
-      // return this.sprites[this.currentIndex];
-      return this.sprites[this.currentIndex];
+    props: {
+        pokemon: { type: Object }
     },
-    capitalizedName: function() {
-      return (
-        this.pokemon.name.charAt(0).toUpperCase() + this.pokemon.name.slice(1)
-      );
-    },
-    sprites: function() {
-      return this.pokemon.sprites;
-    }
-  },
-  methods: {
-    nextImage: function() {
-      this.currentIndex = (this.currentIndex + 1) % this.sprites.length;
-    },
-    previousImage: function() {
-      this.currentIndex = (this.currentIndex - 1) % this.sprites.length;
-      if (this.currentIndex < 0) {
-        this.currentIndex = this.sprites.length - 1;
-      }
-    }
-  },
+    components: { PokemonBoxPaginer },
 
-  mounted() {
-    global.pokemon = this.pokemon;
-  }
+    data() {
+        return {
+            currentIndex: 0
+        };
+    },
+    computed: {
+        currentImage: function() {
+            // return this.sprites[this.currentIndex];
+            return this.sprites[this.currentIndex];
+        },
+        capitalizedName: function() {
+            return (
+                this.pokemon.name.charAt(0).toUpperCase() +
+                this.pokemon.name.slice(1)
+            );
+        },
+        sprites: function() {
+            return this.pokemon.sprites;
+		},
+		spritesLength: function () {
+			return this.sprites.length;
+		}
+    },
+    methods: {
+        updateIndex: function(value, event) {
+			this.currentIndex = value;
+			if(event) event.preventDefault();
+        }
+    },
+
+    mounted() {
+    }
 };
 </script>
 
 <style>
 .box {
-  text-align: center;
-  border: 1px solid black;
+    text-align: center;
+    border: 1px solid black;
 }
 
 .box .list li {
-  list-style-type: square;
-  text-align: justify;
+    list-style-type: square;
+    text-align: justify;
 }
 </style>
---
+++ src/components/PokemonBoxPaginer.vue
@@ -0,0 +1,58 @@
+<template>
+  <nav aria-label="Page navigation example">
+    <ul class="pagination d-flex justify-content-center">
+      <li class="page-item">
+        <a class="page-link" href="#" aria-label="Previous">
+          <span aria-hidden="true" @click="previousImage">&laquo;</span>
+          <span class="sr-only">Previous</span>
+        </a>
+      </li>
+      <li class="page-item" v-for="index in nbrImages" :key="index">
+        <a class="page-link" href="#" @click="(e) => updateIndex(e, index - 1)">{{ index }}</a>
+      </li>
+      <li class="page-item">
+        <a class="page-link" href="#" aria-label="Next">
+          <span aria-hidden="true" @click="nextImage">&raquo;</span>
+          <span class="sr-only">Next</span>
+        </a>
+      </li>
+    </ul>
+  </nav>
+</template>
+
+<script>
+
+    export default {
+        props: {
+            nbrImages: { type: Number }
+        },  
+        data () {
+            return {
+                currentIndex: 0,
+            };
+        },
+        methods: {
+            previousImage: function (event) {
+                event.preventDefault();
+                this.currentIndex = (this.currentIndex - 1) % this.nbrImages;
+                if (this.currentIndex < 0) {
+                    this.currentIndex = this.nbrImages - 1;
+                }
+                this.$emit('update-index', this.currentIndex);
+            },
+            nextImage: function (event) {
+                event.preventDefault();
+                this.currentIndex = (this.currentIndex + 1) % this.nbrImages;
+                this.$emit('update-index', this.currentIndex);
+            },
+            updateIndex: function (event, index) {
+                event.preventDefault();
+                this.currentIndex = index;
+                this.$emit('update-index', this.currentIndex);
+            }
+        }, 
+        mounted () {
+        }
+    }
+
+</script>
