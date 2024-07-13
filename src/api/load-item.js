af7aea6652dfaec6dbffb466fe29972318dd00e4
dterbah <dterbah@hevaweb.com>
2024-07-13 20:11:37 +0200

    feat: Remove console.log

--- src/api/load-item.js
+++ src/api/load-item.js
@@ -1,39 +1,37 @@
-import axios from 'axios';
+import axios from "axios";
 
-import pokeStore from '../data/AppStore.js';
-import Item from '../data/Item.js';
-import URL from './url.js';
+import pokeStore from "../data/AppStore.js";
+import Item from "../data/Item.js";
+import URL from "./url.js";
 
 // function that load an item by its id
 const itemLoader = async (itemId) => {
-    let result = pokeStore.getters.getItemByid(itemId);
-    console.log(result);
-    if(result) return undefined;
+  let result = pokeStore.getters.getItemByid(itemId);
+  if (result) return undefined;
 
-    let url = URL.itemLink + itemId;
-    const itemRequestData = await axios.get(url);
-    const rawData = itemRequestData.data;
+  let url = URL.itemLink + itemId;
+  const itemRequestData = await axios.get(url);
+  const rawData = itemRequestData.data;
 
-    const data = {
-        attributes: rawData.attributes.map(attribute => attribute.name),
-        specs: {
-            category: rawData.category.name.sanitize().firstLetterToUpper(),
-            effects: rawData.effect_entries.map(entry => {
-                return {
-                    resume: entry.effect,
-                    short_effect: entry.language.short_effect
-                };
-            }),
-            cost: rawData.cost,
+  const data = {
+    attributes: rawData.attributes.map((attribute) => attribute.name),
+    specs: {
+      category: rawData.category.name.sanitize().firstLetterToUpper(),
+      effects: rawData.effect_entries.map((entry) => {
+        return {
+          resume: entry.effect,
+          short_effect: entry.language.short_effect,
+        };
+      }),
+      cost: rawData.cost,
+    },
+    id: rawData.id,
+    name: rawData.name.firstLetterToUpper().sanitize(),
+    sprite: rawData.sprites.default,
+  };
 
-        },
-        id: rawData.id,
-        name: rawData.name.firstLetterToUpper().sanitize(),
-        sprite: rawData.sprites.default
-    }
-    
-    const item = new Item(data);
-    return item;
-}
+  const item = new Item(data);
+  return item;
+};
 
 export default itemLoader;
